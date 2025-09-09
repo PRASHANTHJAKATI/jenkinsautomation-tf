32a002c1a040368f367dba54bca656f927c8a37c
PRASHANTHJAKATI <166536642+PRASHANTHJAKATI@users.noreply.github.com>
2025-09-09 10:04:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,9 +4,12 @@ provider "aws" {
 
 data "aws_ami" "dev_ami" {
   most_recent = true
+
   filter {
     name   = "tag:Env"
     values = ["dev"]
   }
 
+  owners = ["self"] # options: "self", "amazon", or AWS account ID
 }
+
